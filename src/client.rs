dc042d1c38ccb506079ea5bc9d117cf4bfa3b25d
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2024-09-03 11:41:06 +0200

    trigger unavailable error

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,15 @@
 use tonic::transport::Channel;
 use crate::info::info_service_client::InfoServiceClient;
 use crate::info::InfoRequest;
+use crate::status::status_service_client::StatusServiceClient;
+use crate::status::StatusRequest;
 
 pub mod info {
     tonic::include_proto!("info");
+}
+
+pub mod status {
+    tonic::include_proto!("status");
 }
 
 #[tokio::main]
@@ -14,15 +20,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let interval_string = std::env::var("INTERVAL").unwrap_or("3".to_string());
     let interval = interval_string.parse::<u64>()?;
 
-    let mut client = InfoServiceClient::connect(url).await?;
+    let mut info_client = InfoServiceClient::connect(url.clone()).await?;
+    let mut status_client = StatusServiceClient::connect(url.clone()).await?;
 
     loop {
-        let (elapsed, result) = time_async_call(get_hostname(&mut client))
+        let (elapsed_info, result_info) = time_async_call(get_hostname(&mut
+            info_client))
             .await;
 
-        let hostname = result?;
+        let (elapsed_status, _) = time_async_call(invoke_status_code
+            (&mut
+            status_client))
+            .await;
 
-        println!("{:?} ({}ms)", hostname, elapsed);
+        let hostname = result_info?;
+
+        println!("Hostname : {:?} ({}ms)", hostname, elapsed_info);
+        println!("Status   : {}ms", elapsed_status);
 
         tokio::time::sleep(tokio::time::Duration::from_secs(interval)).await;
     }
@@ -34,6 +48,16 @@ async fn get_hostname(client: &mut InfoServiceClient<Channel>) -> Result<String,
     let response = client.info(request).await?;
 
     Ok(response.into_inner().hostname)
+}
+
+async fn invoke_status_code(client: &mut StatusServiceClient<Channel>) -> Result<(), Box<dyn std::error::Error>> {
+    let request = tonic::Request::new(StatusRequest {
+        code: "Unavailable".to_string()
+    });
+
+    client.status(request).await?;
+
+    Ok(())
 }
 
 async fn time_async_call<F, T>(
